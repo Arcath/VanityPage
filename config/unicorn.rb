f598860bb12eb08d45562a220de216a40cc8778b
Arcath <adam@arcath.net>
2012-10-21 15:59:15 +0100

    Tweak the fonts

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,7 +1,7 @@
 working_directory "/mina/vanity/current"
 pid "/mina/vanity/shared/tmp/pids/unicorn.pid"
 stderr_path "/mina/vanity/shared/log/unicorn.log"
-stdout_path "/mina/vanity/log/unicorn.log"
+stdout_path "/mina/vanity/shared/log/unicorn.log"
 
 listen "/tmp/unicorn.vanity.sock"
 worker_processes 2
